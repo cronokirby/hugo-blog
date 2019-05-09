215df5abc77e00d2d49b7fe61f70d51325c0260f
Lúcás Meier <cronokirby@gmail.com>
2019-05-09 19:49:23 +0200

    Deploy

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,25 @@
-STATUS="$(git status)"
+#!/bin/sh
 
-if [[ $STATUS == *"nothing to commit, working tree clean"* ]]
+if [[ $(git status -s) ]]
 then
-    hugo
-    sed -i '/public/d' ./.gitignore
-    git add .
-    git commit -m "Edit .gitignore to publish"
-    git push origin `git subtree split --prefix public master`:gh-pages
-    git reset HEAD~
-    git checkout .gitignore
-else
-    echo "Need clean working directory to publish"
+    echo "The working directory is dirty. Please commit any pending changes."
+    exit 1;
 fi
+
+echo "Deleting old publication"
+rm -rf public
+mkdir public
+git worktree prune
+rm -rf .git/worktrees/public/
+
+echo "Checking out gh-pages branch into public"
+git worktree add -B gh-pages public upstream/gh-pages
+
+echo "Removing existing files"
+rm -rf public/*
+
+echo "Generating site"
+hugo
+
+echo "Updating gh-pages branch"
+cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
