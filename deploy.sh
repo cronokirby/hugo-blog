41c0d227e6d5f783ae1a226490c574d18daaddf4
Lúcás Meier <cronokirby@gmail.com>
2019-05-09 19:33:45 +0200

    deploy

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ then
     sed -i '/public' ./.gitignore
     git add .
     git commit -m "Edit .gitignore to publish"
-    git push origin `git subtree split --prefix publi master`:gh-pages --force
+    git push origin `git subtree split --prefix public master`:gh-pages --force
     git reset HEAD~
     git checkout .gitignore
 else
